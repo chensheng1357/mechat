76ad53f7448656117a377bcdfb17ff37862eefb8
chensheng1357 <cs200521@163.com>
2014-06-19 22:09:25 +0800

    增加禁止横屏

--- mechat/CoAppDelegate.m
+++ mechat/CoAppDelegate.m
@@ -60,4 +60,11 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+// 禁止横屏
+- (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+
 @end
